dc67b599b0460619ee20613df8ba50c48b237525
Cesar Martinez <cesaguma@hotmail.com>
2020-10-26 22:35:20 -0400

    Adding comments and describing how the code works

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-const employeesArray = []; // Declaring array of objects
+const employeesArray = []; // Declaring an array of objects
 
 console.log( // Creating a logo for the app using the package 'asciiart-logo'
     logo({
@@ -34,9 +34,6 @@ console.log( // Creating a logo for the app using the package 'asciiart-logo'
     .center('Author: Cesar A Martinez')
     .render()
 );
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
 
 function promptUser(){
     inquirer.prompt([
@@ -68,20 +65,16 @@ function promptUser(){
         }
     ]).then(function(data) {
         let manager = new Manager(data.name,data.id,data.email,data.officeNumber);
-        employeesArray.push(manager);
+        employeesArray.push(manager);  // Adding a new object into an array.
         if (data.options === "Engineer"){
             promtEngineer();                 
         } else if (data.options === "Intern"){
            promptIntern();
         } else {
             console.log(employeesArray);
-            //render(employeesArray);
-            fs.writeFileSync(outputPath,render(employeesArray));                
+            fs.writeFileSync(outputPath,render(employeesArray));// Calling the render function and passing an array containing all employee objects. 
+            //Creating an HTML file named 'team.html'               
         }
-        
-        
-
-
     });        
 }
 
@@ -117,14 +110,15 @@ function  promtEngineer(){
         }
     ]).then(function(data) {
         let engineer = new Engineer(data.name,data.id,data.email,data.github);
-        employeesArray.push(engineer);
+        employeesArray.push(engineer);  // Adding a new object into an array.
         if (data.options === "Engineer"){
             promtEngineer();                 
         } else if (data.options === "Intern"){
            promptIntern();
         } else {
             console.log(employeesArray);
-            fs.writeFileSync(outputPath,render(employeesArray));     
+            fs.writeFileSync(outputPath,render(employeesArray)); // Calling the render function and passing an array containing all employee objects. 
+            //Creating an HTML file named 'team.html'          
         }
     });
 }
@@ -158,35 +152,15 @@ function promptIntern(){
         }
     ]).then(function(data) {
         let intern = new Intern(data.name,data.id,data.email,data.school);
-        employeesArray.push(intern);
+        employeesArray.push(intern); // Adding a new object into an array.
         if (data.options === "Engineer"){
             promtEngineer();                 
         } else if (data.options === "Intern"){
            promptIntern();
         } else {
             console.log(employeesArray);
-            //console.log(render(employeesArray));
-            fs.writeFileSync(outputPath,render(employeesArray));      
+            fs.writeFileSync(outputPath,render(employeesArray)); // Calling the render function and passing an array containing all employee objects. 
+            //Creating an HTML file named 'team.html'      
         }
     });
 }
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
