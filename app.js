e592675e80b19b4d41becb6a6286c93ad1a86359
Cesar Martinez <cesaguma@hotmail.com>
2020-10-27 22:48:25 -0400

    Adding functions to validate the value from user's inputs

--- app.js
+++ app.js
@@ -17,6 +17,48 @@ const render = require("./lib/htmlRenderer");
 
 const employeesArray = []; // Declaring an array of objects
 
+const valName = async (input) => {
+    if (input === '' || /^\s+$/.test(input)) {
+       return 'Incorrect asnwer. It must contain at least a character';
+    }
+    return true;
+ };
+
+ const valId = async (input) => {
+    if (input === '' || /^\s+$/.test(input) || !isNumber(input)) {
+       return 'Incorrect asnwer. It must contain at least a number';
+    }
+    return true;
+ }; 
+ function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+const valEmail = async (input) => {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(input)) {
+       return 'Incorrect asnwer.It is not a valid e-mail. Format: xxxx@xxxx.xxx';
+    }
+    return true;
+ }; 
+
+ const valNumber = async (input) => {
+    const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    if (!re.test(input)) {
+       return 'Incorrect asnwer.It is not a valid Phone Number. Format: 111-111-1111 or (111) 111-1111';
+    }
+    return true;
+ }; 
+
+ const valGitHub = async (input) => {
+    const re = /\B@((?!.*(-){2,}.*)[a-z0-9][a-z0-9-]{0,38}[a-z0-9])/ig;
+    if (!re.test(input)) {
+       return 'Incorrect asnwer.It is not a valid GitHub username. Format: @valid or @valid-username';
+    }
+    return true;
+ }; 
+
+
 console.log( // Creating a logo for the app using the package 'asciiart-logo'
     logo({
         name: 'Template Enginee Generator',
@@ -40,22 +82,26 @@ function promptUser(){
         {
             type: "input",
             name: "name",
-            message: "What is your Manager's name?"                
+            message: "What is your Manager's name?",
+            validate: valName
         },
         {
             type: "input",
             name: "id",
-            message: "What is your Manager's id?"                   
+            message: "What is your Manager's id?",
+            validate: valId                   
         },
         {
             type: "input",
             name: "email",
-            message: "What is your Manager's email?"                   
+            message: "What is your Manager's email?",
+            validate: valEmail                   
         },
         {
             type: "input",
             name: "officeNumber",
-            message: "What is your Manager's office number?"                
+            message: "What is your Manager's office number?",
+            validate: valNumber                
         },
         {
             type: "list",
@@ -85,22 +131,26 @@ function  promtEngineer(){
         {
             type: "input",
             name: "name",
-            message: "What is your Engineer's name?"                
+            message: "What is your Engineer's name?",
+            validate: valName                      
         },
         {
             type: "input",
             name: "id",
-            message: "What is your Engineer's id?"                   
+            message: "What is your Engineer's id?",
+            validate: valId                   
         },
         {
             type: "input",
             name: "email",
-            message: "What is your Engineer's email?"                   
+            message: "What is your Engineer's email?",
+            validate: valEmail                   
         },
         {
             type: "input",
             name: "github",
-            message: "What is your Engineer's Github username?"                
+            message: "What is your Engineer's Github username?",
+            validate: valGitHub                
         },
         {
             type: "list",
@@ -127,22 +177,26 @@ function promptIntern(){
         {
             type: "input",
             name: "name",
-            message: "What is your Intern's name?"                
+            message: "What is your Intern's name?",
+            validate: valName                
         },
         {
             type: "input",
             name: "id",
-            message: "What is your Intern's id?"                   
+            message: "What is your Intern's id?",
+            validate: valId                   
         },
         {
             type: "input",
             name: "email",
-            message: "What is your Intern's email?"                   
+            message: "What is your Intern's email?",
+            validate: valEmail                   
         },
         {
             type: "input",
             name: "school",
-            message: "What is your Intern's school?"                
+            message: "What is your Intern's school?",
+            validate: valName                
         },
         {
             type: "list",
